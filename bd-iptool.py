fbd115633b37fab6b4c37c93211d9e372395c128
boldi-kdg <boldizsar.olajos@student.kdg.be>
2022-12-05 22:37:55 +0100

    Fixes and cleanup

--- bd-iptool.py
+++ bd-iptool.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# https://github.com/boldi-kdg/bd-iptool #
 _CURRENT_VERSION = '1.0'
 
 import argparse
@@ -84,6 +85,22 @@ class IPAddress:
             # default to Class C
             return 24
     
+    def get_default_class(self) -> str:
+        if self.value[0] >= int('0b0000_0000', base=0) and self.value[0] <= int('0b0111_1111', base=0):
+            return 'Class A'
+        elif self.value[0] >= int('0b1000_0000', base=0) and self.value[0] <= int('0b1011_1111', base=0):
+            return 'Class B'
+        elif self.value[0] >= int('0b1100_0000', base=0) and self.value[0] <= int('0b1101_1111', base=0):
+            return 'Class C'
+        elif self.value[0] >= int('0b1110_0000', base=0) and self.value[0] <= int('0b1110_1111', base=0):
+            return 'Class D (Multicast or Experimental) [WARNING: Currently handled by the tool as Class C]'
+        elif self.value[0] >= int('0b1111_0000', base=0) and self.value[0] <= int('0b1111_1111', base=0):
+            if self.value[0] == 255 and self.value[1] == 255 and self.value[2] == 255 and self.value[3] == 255:
+                return 'BROADCAST (Unique adress, would fall under Class E technically) [WARNING: Currently handled by the tool as Class C]'
+            return 'Class E (Experimental) [WARNING: Currently handled by the tool as Class C]'
+        else:
+            return 'Unknown'
+    
     def get_binary(self):
         res = ''
         for i, byte in enumerate(self.value):
@@ -123,7 +140,7 @@ class IPAddress:
         return IPAddress(bitwise_or(nwaddr, invmask), self.prefix)
     
     def get_address_type(self):
-        return 'Unknown'
+        return 'Unknown (WIP)'
         nwaddr = self.get_network_address()
         bcaddr = self.get_broadcast_address()
         if self == nwaddr:
@@ -144,7 +161,7 @@ class IPAddress:
         print(f"Network Address: {str(self.get_network_address())}")
         print(f"Broadcast Address: {str(self.get_broadcast_address())}")
         print(f"Subnet Mask: {self.get_subnet_mask()}")
-        print(f"Address Type: {self.get_address_type()}")
+        print(f"Default Class: {self.get_default_class()}")
 
     @staticmethod
     def from_dotted_binary(arg: str):
@@ -208,8 +225,7 @@ class IPAddress:
                     raise argparse.ArgumentTypeError(f"All octets must be in the unsigned byte range (0-255).\n Octet #{i+1} is outside this range with the value of: {val}")
                 octet_values[i] = val
             except ValueError:
-                ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
-                raise argparse.ArgumentTypeError(f"Failed to convert the {ordinal(i+1)} octet to a number: '{octet}'")
+                raise argparse.ArgumentTypeError(f"Failed to convert octet #{i+1} to a number: '{octet}'")
         # finally return a new IPAddress object
         return IPAddress(octet_values, prefix)
 
@@ -222,39 +238,74 @@ parser_convert = subcommands.add_parser('convert',description="Convert ip adress
 parser_convert.add_argument('-d', metavar='ip-address', dest='ipaddr', type=IPAddress.from_dotted_decimal)
 parser_convert.add_argument('-b', metavar='ip-address', dest='ipaddr', type=IPAddress.from_dotted_binary)
 
-parser_subnet = subcommands.add_parser('subnet',description="Split a network into subnets according to the given requirements.")
+parser_subnet = subcommands.add_parser('subnet',description="Split a network into subnets according to the given requirements.", epilog='''
+table columns:
+Hb = host bits required, this is calculated from requiredHosts;
+Bb = the bits borrowed, aka. additional subnet bits required;
+# of IPs = yes this is the number of IPs on this subnet,
+  KEEP IN MIND: 2 of these IPs are not usable for hosts (network address & host address);
+InR = in range, if its a cross it indicates the subnet is not part of the original network, 
+  meaning you ran out of IP adresses and cannot make more subnets (you may still try smaller subnets.);
+''')
 parser_subnet.add_argument('ipaddr', metavar='ip-address', type=IPAddress.from_dotted_decimal)
-parser_subnet.add_argument('-s', '--required-subnets', )
+parser_subnet.add_argument('-F', action='store_true', default=False, dest='fixedLength', help='fixed length subnetting: make subnets with length requiredHosts[0] until a subnet is out of range')
+parser_subnet.add_argument('-s', action='store_true', default=False, dest='subnetsInsteadOfHosts', help='requiredHosts will be interpreted as requiredSubnets instead')
 parser_subnet.add_argument('requiredHosts', type=int, nargs='+')
 
 def convert(args):
     print("--- CONVERT ---")
-    print(args)
+    #print(args)
+    if args.ipaddr == None:
+        print(" You must specify the ip address to convert, in binary with -b or in decimal with -d.")
+        return
+    
     print(f'DECIMAL: {repr(args.ipaddr)}')
     print(f'BINARY: {args.ipaddr.get_binary()}')
+    print("--- IP CHARACTERISTICS ---")
+    args.ipaddr.print_characteristics()
 
 def subnet(args):
     print("--- IP CHARACTERISTICS ---")
     args.ipaddr.print_characteristics()
     print("--- SUBNETTING ---")
-    print(args)
-    requiredHosts = sorted(args.requiredHosts, reverse=True)
+    #print(args)
     currNw = args.ipaddr.get_network_address()
     numIps = 0
-    hostBits = []
-    bitsToBorrow = []
-    print('{:3s}|{:2s}|{:15s}|{:17s}|{:17s}|{:8s}|'.format('SN#', 'bB', 'new subnet mask', 'network address', 'broadcast address', '# of IPs'))
-    for i, rh in enumerate(requiredHosts):
-        hostBits.append(math.ceil(math.log(rh+2, 2)))
-        bitsToBorrow.append(32-(args.ipaddr.prefix + hostBits[i]))
-        currNw = IPAddress((currNw + numIps).value, args.ipaddr.prefix + bitsToBorrow[i])
-        numIps = 2 ** hostBits[i]
-        sameNetCheck = '❌'
-        if args.ipaddr.get_network_address() == IPAddress(currNw.value, args.ipaddr.prefix).get_network_address():
-            sameNetCheck = '✔️'
+    print('{:3s}|{:2s}|{:2s}|{:15s}|{:18s}|{:18s}|{:8s}|{:8s}|{:3s}'.format('SN#', 'Hb', 'Bb', 'new subnet mask', 'network address', 'broadcast address', '# of IPs', '# of SNs', 'InR'))
+    if args.fixedLength:
+        i = 0
+        passedCheck = True
+        while passedCheck:
+            currNw, numIps, passedCheck = subnet_gensubnet(args, i+1, args.requiredHosts[0], currNw, numIps)
+            i+=1
+    else:
+        requiredHosts = sorted(args.requiredHosts, reverse=(not args.subnetsInsteadOfHosts))
+        for i, rh in enumerate(requiredHosts):
+            currNw, numIps, _ = subnet_gensubnet(args, i+1, rh, currNw, numIps)
+        
+def subnet_gensubnet(args, sn: int, rh: int, currNw: IPAddress, numIps: int):
+    if args.subnetsInsteadOfHosts:
+        bitsToBorrow = math.ceil(math.log(rh, 2))
+        hostBits = 32-(args.ipaddr.prefix + bitsToBorrow)
+    else:
+        hostBits = math.ceil(math.log(rh+2, 2))
+        bitsToBorrow = 32-(args.ipaddr.prefix + hostBits)
+    currNw = IPAddress((currNw + numIps).value, args.ipaddr.prefix + bitsToBorrow)
+    numIps = 2 ** hostBits
+    numSNs = 2 ** bitsToBorrow
+    sameNetCheck = '❌'
+    inRange = False
+    if args.ipaddr.get_network_address() == IPAddress(currNw.value, args.ipaddr.prefix).get_network_address():
+        sameNetCheck = '✔️'
+        inRange = True
+    
+    print('{:3d}|{:2d}|{:2d}|{:15s}|{:18s}|{:18s}|{:8d}|{:8d}|{:3s}'.format(sn, hostBits, bitsToBorrow, currNw.get_subnet_mask(), str(currNw.get_network_address()), str(currNw.get_broadcast_address()), numIps, numSNs, sameNetCheck))
+    return (currNw, numIps, inRange)
 
-        print('{:3d}|{:2d}|{:15s}|{:17s}|{:17s}|{:8d}|{:1s}'.format(i+1, bitsToBorrow[i], currNw.get_subnet_mask(), str(currNw.get_network_address()), str(currNw.get_broadcast_address()), numIps, sameNetCheck))
 
 if __name__=='__main__':
     parsedArgs = parser.parse_args()
-    locals()[parsedArgs.func](parsedArgs)
+    if parsedArgs.func not in ('convert', 'subnet'):
+        parser.print_help()
+    else:
+        locals()[parsedArgs.func](parsedArgs)
